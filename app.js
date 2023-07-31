2aba671a58959c9b9c18fc6dbe60f81523811129
olareign <abdulrasaqolarewaju88@gmail.com>
2023-07-31 13:27:11 +0100

    second app with johnSmilga udemy course

---
+++ app.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+require('express-async-errors');
+
+
+const path = require('path')
+// extra security packages
+const helmet = require('helmet');
+const xss = require('xss-clean');
+
+// {
+  // the below line of code is needed to be removed since we are having our frontend module present
+  //Swagger 
+  // const swaggerUI = require('swagger-ui-express');
+  // const YAML = require('yamljs');
+  // const swaggerDocument = YAML.load('./swagger.yaml');
+// }
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.json());
+app.use(helmet());
+
+app.use(xss());
+
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+
+// serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+// console.clear()
+start();
